9beba0729cf36d10489a26ae4b8debd10f08696f
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-06-14 09:46:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# freeCodeCamp Product Landing Page Project
+
+This single-page website is my third of five final projects for freeCodeCamp's
+Responsive Web Design course. It's a product landing page for the fictitious
+company, Original Trombones.
---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * This function adjusts the top margin of a non-fixed element in a container
+ * with a fixed one above it so that the fixed element doesn't possibly overlap
+ * with it.
+ * @param {htmlElement} fixedElement
+ * @param {htmlElement} notFixedElement
+ */
+function adjustNotFixedElement(fixedElement, notFixedElement) {
+  notFixedElement.style.marginTop = fixedElement.offsetHeight + 'px';
+}
+
+/**
+ * Modifies the CSS of a given element so that it's an
+ * [obround](https://www.wordnik.com/words/obround). The function does so by
+ * setting the `border-radius`, `padding-right`, and `padding-left` properties
+ * to half of the element's height.
+ * @param {HTMLElement} htmlElement - HTML element to change
+ */
+function makeElementObround(htmlElement) {
+  const HALF_HEIGHT = ~~(0.5 * htmlElement.offsetHeight) + 'px';
+  htmlElement.style.borderRadius = HALF_HEIGHT;
+  htmlElement.style.paddingRight = HALF_HEIGHT;
+  htmlElement.style.paddingLeft = HALF_HEIGHT;
+}
+
+const projectName = 'product-landing-page';
+
+const LOGO_AND_NAV = document.getElementById('logo-and-nav');
+const TITLE_AND_SUBTITLE = document.getElementById('title-and-subtitle');
+
+adjustNotFixedElement(LOGO_AND_NAV, TITLE_AND_SUBTITLE);
+
+window.addEventListener('resize', function () {
+  adjustNotFixedElement(LOGO_AND_NAV, TITLE_AND_SUBTITLE);
+});
+
+makeElementObround(document.getElementById('email'));
+makeElementObround(document.getElementById('submit'));
